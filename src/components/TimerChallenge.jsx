402deb2e3b4fa7e01a48bd646f28d3b1dddf7503
manu.m <manu.m@tataelxsi.co.in>
2025-04-01 13:02:37 +0530

    completed handling timer using ref

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,18 +1,42 @@
+import { useState, useRef } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+    const timer = useRef();
+
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+
+    function handleStart() {
+        console.log('Starting timer')
+        timer.current = setTimeout(() => {
+            setTimerExpired(true)
+
+        }, targetTime * 1000)
+        setTimerStarted(true);
+    }
+
+    function handleStop() {
+        console.log('Stopping timer')
+        clearTimeout(timer.current)
+        setTimerStarted(false);
+        // setTimerExpired(false);
+    }
     return (
         <>
             <section className="challenge">
                 <h2>{title}</h2>
+                {timerExpired && <p>You lost !</p>}
                 <p className="chalenge-time">
-                    {targetTime} seconds{targetTime >1 ? 's' : ''}
+                    {targetTime} seconds{targetTime > 1 ? 's' : ''}
                 </p>
                 <p>
-                    <button>
-                        Start Challenge
+                    <button onClick={timerStarted ? handleStop : handleStart}>
+                        {timerStarted ? 'Stop' : 'Start'} Challenge
                     </button>
                 </p>
-                <p className="">
-                    Time is running .... / Timer inactive
+                <p className={timerStarted ? 'active' : undefined}>
+                    {timerStarted ? 'Time is running' : 'Timer inactive'}
                 </p>
             </section>
         </>
